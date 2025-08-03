1b622394be3f22ca5c1c65bde18bd2dc34d75875
ZeynepAtes <ZeynepAtes@AG000NB160>
2025-08-03 14:07:36 +0300

    Add project files.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryEFApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEFApp.Data
+{
+    public class LibraryContext: DbContext
+    {
+        public DbSet<Book> Books => Set<Book>();
+        public LibraryContext() { }
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;
+                                        Database=LibraryDb;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using LibraryEFApp.Data;
+using LibraryEFApp.Models;
+
+var context = new LibraryContext();
+int choice;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("\n--- Library Management System ---");
+    Console.WriteLine("1. List all books");
+    Console.WriteLine("2. Add new book");
+    Console.WriteLine("3. Update book");
+    Console.WriteLine("4. Delete book");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choice: ");
+    choice = Console.ReadKey(true).KeyChar - '0';
+    Console.WriteLine();
+
+    switch (choice)
+    {
+        case 1:
+            Console.Clear();
+            ListBooks();
+
+            Console.Write("Press any key to return to menu...");
+            Console.ReadKey();
+            break;
+        case 2:
+            Console.Clear();
+            AddBook();
+
+            Console.Write("Press any key to return to menu...");
+            Console.ReadKey();
+            break;
+        case 3:
+            Console.Clear();
+            UpdateBook();
+
+            Console.Write("Press any key to return to menu...");
+            Console.ReadKey();
+            break;
+        case 4:
+            Console.Clear();
+            DeleteBook();
+
+            Console.Write("Press any key to return to menu...");
+            Console.ReadKey();
+            break;
+        case 0:
+            Console.WriteLine("Goodbye...");
+            break;
+        default:
+            Console.WriteLine("Invalid choice!");
+
+            Console.Write("Press any key to selection...");
+            Console.ReadKey();
+            break;
+    }
+} while (choice != 0);
+
+void ListBooks()
+{
+    var books = context.Books.ToList();
+    Console.WriteLine("\n--- Book List ---");
+    foreach (var book in books)
+    {
+        Console.WriteLine($"{book.Id}: {book.Author} - {book.Title} ({book.Year})");
+    }
+}
+
+void AddBook()
+{
+    Console.Write("Title: ");
+    String title = Console.ReadLine();
+
+    Console.Write("Author: ");
+    String author = Console.ReadLine();
+
+    Console.Write("Year: ");
+    int.TryParse(Console.ReadLine(), out int year);
+
+    var book = new Book
+    {
+        Title = title,
+        Author = author,
+        Year = year
+    };
+    context.Books.Add( book );
+    context.SaveChanges();
+    Console.WriteLine("Book has been added");
+}
+
+void UpdateBook()
+{
+    ListBooks();
+    Console.Write("Choose a book to update: ");
+    int id = Console.ReadKey(true).KeyChar - '0';
+    Console.WriteLine();
+
+    var book = context.Books.Find(id);
+    if (book == null)
+    {
+        Console.WriteLine("Invalid choice!");
+        return;
+    }
+
+    Console.WriteLine("\n--- Update Options ---\n1: Title\n2: Author\n3: Year");
+    Console.Write("Choose one to update: ");
+    int option = Console.ReadKey(true).KeyChar - '0';
+    Console.WriteLine();
+
+    switch(option)
+    {
+        case 1:
+            Console.Write("New Title: ");
+            book.Title = Console.ReadLine();
+
+            context.SaveChanges();
+            Console.WriteLine("Book has been updated.");
+            break;
+        case 2:
+            Console.Write("New Author: ");
+            book.Author = Console.ReadLine();
+
+            context.SaveChanges();
+            Console.WriteLine("Book has been updated.");
+            break;
+        case 3:
+            Console.Write("New Year: ");
+            int.TryParse(Console.ReadLine(), out int year);
+            book.Year = year;
+
+            context.SaveChanges();
+            Console.WriteLine("Book has been updated.");
+            break;
+        default:
+            Console.WriteLine("Invalid choice!");
+            break;
+    }
+}
+
+void DeleteBook()
+{
+    ListBooks();
+    Console.Write("Choose a book to delete: ");
+    int id = Console.ReadKey(true).KeyChar - '0';
+    Console.WriteLine();
+
+    var book = context.Books.Find(id);
+    if (book == null)
+    {
+        Console.WriteLine("Invalid choice!");
+        return;
+    }
+
+    context.Books.Remove(book);
+    context.SaveChanges();
+    Console.WriteLine("Book has been deleted");
+}
